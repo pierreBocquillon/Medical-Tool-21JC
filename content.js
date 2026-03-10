ce1bbc5c8229f8d896c7f61612aaa943ba1169ea
BOCQUILLON Pierre <bocquillon.pierre@orange.fr>
2026-03-10 18:24:56 +0100

    fix btn + add red on info pas ok

--- content.js
+++ content.js
@@ -171,7 +171,6 @@
 	// --- Specific Logic ---
 
 	function addBlessures(blessures) {
-		if (blessures === "VC") return // Cas spécial géré ailleurs ou ignoré pour le moment si pas de blessures VC
 		const field = document.querySelector('textarea[name="injuriesRemark"]')
 		if (!field) return
 
@@ -1006,7 +1005,7 @@
 		const btn = createElement("button", {
 			text: "🩸 Groupe sanguin aléatoire",
 			id: "modif-blood-btn",
-			type: "button",
+			attributes: { type: "button" },
 			style: { marginTop: "4px", backgroundColor: "#2a2a2a", color: "#eee", border: "1px solid #555", padding: "4px 10px", borderRadius: "4px", cursor: "pointer", fontSize: "16px", width: "100%" },
 			events: {
 				click: () => {
@@ -1037,7 +1036,6 @@
 		const btn = createElement("button", {
 			text: label,
 			id: id,
-			type: "button",
 			style: { marginTop: "4px", backgroundColor: "#2a2a2a", color: "#eee", border: "1px solid #555", padding: "4px 10px", borderRadius: "4px", cursor: "pointer", fontSize: "16px", width: "100%" },
 			events: {
 				click: () => {
@@ -1058,6 +1056,36 @@
 		input.parentElement.parentElement.appendChild(wrapper)
 	}
 
+	function highlightErrorText() {
+		const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false)
+		const nodes = []
+		while (walker.nextNode()) {
+			const node = walker.currentNode
+			if (node.parentNode.dataset && node.parentNode.dataset.errorHighlight) continue
+			if (node.nodeValue.toLowerCase().includes("infos pas ok")) {
+				nodes.push(node)
+			}
+		}
+
+		nodes.forEach((node) => {
+			const span = document.createElement("span")
+			span.dataset.errorHighlight = "true"
+			const parts = node.nodeValue.split(/(infos pas ok)/gi)
+			parts.forEach((part) => {
+				if (part.toLowerCase() === "infos pas ok") {
+					const b = document.createElement("b")
+					b.textContent = part
+					b.style.color = "red"
+					b.style.whiteSpace = "nowrap"
+					span.appendChild(b)
+				} else if (part) {
+					span.appendChild(document.createTextNode(part))
+				}
+			})
+			node.replaceWith(span)
+		})
+	}
+
 	function injectVCButton() {
 		const divList = document.querySelectorAll('div[role="menu"]')
 		if (divList.length === 0) return
@@ -1072,7 +1100,7 @@
 			const btn = createElement("button", {
 				text: "🔬 VC",
 				id: "modif-vc-btn",
-				type: "button",
+				attributes: { type: "button" },
 				style: { marginTop: "4px", backgroundColor: "#2a2a2a", color: "#eee", border: "1px solid #555", padding: "4px 10px", borderRadius: "4px", cursor: "pointer", fontSize: "16px", width: "100%" },
 				events: {
 					click: () => {
@@ -1123,8 +1151,7 @@
 		if (!parent) return
 
 		const items = [
-			{ hospital: "LSES", title: "Dispatch", link: "https://docs.google.com/spreadsheets/d/1Vho76MbebIo4d1RgpVL0wGFqbMjeK1e3HcirZV_C7Uk/edit?gid=180456797#gid=180456797", icon: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="truck-medical" class="svg-inline--fa fa-truck-medical " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" color="#fff"><path fill="currentColor" d="M0 48C0 21.5 21.5 0 48 0H368c26.5 0 48 21.5 48 48V96h50.7c17 0 33.3 6.7 45.3 18.7L589.3 192c12 12 18.7 28.3 18.7 45.3V256v32 64c17.7 0 32 14.3 32 32s-14.3 32-32 32H576c0 53-43 96-96 96s-96-43-96-96H256c0 53-43 96-96 96s-96-43-96-96H48c-26.5 0-48-21.5-48-48V48zM416 256H544V237.3L466.7 160H416v96zM160 464a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm368-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM176 80v48l-48 0c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h48v48c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V192h48c8.8 0 16-7.2 16-16V144c0-8.8-7.2-16-16-16H240V80c0-8.8-7.2-16-16-16H192c-8.8 0-16 7.2-16 16z"></path></svg>' },
-			{ hospital: "LSES", title: "Stock", link: "https://lses-inventory.web.app/", icon: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="piggy-bank" class="svg-inline--fa fa-piggy-bank " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" color="#fff"><path fill="currentColor" d="M400 96l0 .7c-5.3-.4-10.6-.7-16-.7H256c-16.5 0-32.5 2.1-47.8 6c-.1-2-.2-4-.2-6c0-53 43-96 96-96s96 43 96 96zm-16 32c3.5 0 7 .1 10.4 .3c4.2 .3 8.4 .7 12.6 1.3C424.6 109.1 450.8 96 480 96h11.5c10.4 0 18 9.8 15.5 19.9l-13.8 55.2c15.8 14.8 28.7 32.8 37.5 52.9H544c17.7 0 32 14.3 32 32v96c0 17.7-14.3 32-32 32H512c-9.1 12.1-19.9 22.9-32 32v64c0 17.7-14.3 32-32 32H416c-17.7 0-32-14.3-32-32V448H256v32c0 17.7-14.3 32-32 32H192c-17.7 0-32-14.3-32-32V416c-34.9-26.2-58.7-66.3-63.2-112H68c-37.6 0-68-30.4-68-68s30.4-68 68-68h4c13.3 0 24 10.7 24 24s-10.7 24-24 24H68c-11 0-20 9-20 20s9 20 20 20H99.2c12.1-59.8 57.7-107.5 116.3-122.8c12.9-3.4 26.5-5.2 40.5-5.2H384zm64 136a24 24 0 1 0 -48 0 24 24 0 1 0 48 0z"></path></svg>' },
+			{ hospital: "LSES", title: "LSES Inventory", link: "https://lses-inventory.web.app/", icon: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="truck-medical" class="svg-inline--fa fa-truck-medical " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" color="#fff"><path fill="currentColor" d="M0 48C0 21.5 21.5 0 48 0H368c26.5 0 48 21.5 48 48V96h50.7c17 0 33.3 6.7 45.3 18.7L589.3 192c12 12 18.7 28.3 18.7 45.3V256v32 64c17.7 0 32 14.3 32 32s-14.3 32-32 32H576c0 53-43 96-96 96s-96-43-96-96H256c0 53-43 96-96 96s-96-43-96-96H48c-26.5 0-48-21.5-48-48V48zM416 256H544V237.3L466.7 160H416v96zM160 464a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm368-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM176 80v48l-48 0c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h48v48c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V192h48c8.8 0 16-7.2 16-16V144c0-8.8-7.2-16-16-16H240V80c0-8.8-7.2-16-16-16H192c-8.8 0-16 7.2-16 16z"></path></svg>' },
 			{ hospital: "BCES", title: "Dispatch", link: "https://docs.google.com/spreadsheets/d/1Vho76MbebIo4d1RgpVL0wGFqbMjeK1e3HcirZV_C7Uk/edit?gid=1466825062#gid=1466825062", icon: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="truck-medical" class="svg-inline--fa fa-truck-medical " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" color="#fff"><path fill="currentColor" d="M0 48C0 21.5 21.5 0 48 0H368c26.5 0 48 21.5 48 48V96h50.7c17 0 33.3 6.7 45.3 18.7L589.3 192c12 12 18.7 28.3 18.7 45.3V256v32 64c17.7 0 32 14.3 32 32s-14.3 32-32 32H576c0 53-43 96-96 96s-96-43-96-96H256c0 53-43 96-96 96s-96-43-96-96H48c-26.5 0-48-21.5-48-48V48zM416 256H544V237.3L466.7 160H416v96zM160 464a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm368-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM176 80v48l-48 0c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h48v48c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V192h48c8.8 0 16-7.2 16-16V144c0-8.8-7.2-16-16-16H240V80c0-8.8-7.2-16-16-16H192c-8.8 0-16 7.2-16 16z"></path></svg>' },
 		]
 
@@ -1150,5 +1177,6 @@
 		injectSimpleButton("modif-vm-btn", "⚕️ VM Maintenant", 'input[name="medicalVisitDate"]')
 		injectSimpleButton("modif-dds-btn", "🩸 DDS Maintenant", 'input[name="bloodDonationDate"]', true)
 		injectVCButton()
+		highlightErrorText()
 	}, 500)
 })()
